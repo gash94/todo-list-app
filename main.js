cc3e13d80a9e69cb28e3acaa6222d34c12d8aee2
gash94 <104354207+gash94@users.noreply.github.com>
2022-10-26 20:35:17 +0200

    add sort() todos

--- main.js
+++ main.js
@@ -37,83 +37,85 @@ function displayTodos() {
 
     todoList.innerHTML = "";
 
-    
-    //.sort()
-    todos.forEach((todo) => {
-        const todoItem = document.createElement("div");
-        todoItem.classList.add("todo-item");
+    todos
+        .sort((a, b) => b.createdAt - a.createdAt)
+        .forEach((todo) => {
+            const todoItem = document.createElement("div");
+            todoItem.classList.add("todo-item");
 
-        const label = document.createElement("label");
-        const input = document.createElement("input");
-        const span = document.createElement("span");
-        const content = document.createElement("div");
-        const actions = document.createElement("div");
-        const editBtn = document.createElement("button");
-        const deleteBtn = document.createElement("button");
+            const label = document.createElement("label");
+            const input = document.createElement("input");
+            const span = document.createElement("span");
+            const content = document.createElement("div");
+            const actions = document.createElement("div");
+            const editBtn = document.createElement("button");
+            const deleteBtn = document.createElement("button");
 
-        input.type = "checkbox";
-        input.checked = todo.done;
-        span.classList.add("bubble");
+            input.type = "checkbox";
+            input.checked = todo.done;
+            span.classList.add("bubble");
 
-        if (todo.category == "personal") {
-            span.classList.add("personal");
-            todoItem.style.backgroundColor = "rgba(58, 130, 238, 0.2)";
-        } else {
-            span.classList.add("business");
-            todoItem.style.backgroundColor = "rgba(241, 38, 72, 0.2)";
-        }
+            if (todo.category == "personal") {
+                span.classList.add("personal");
+                todoItem.style.backgroundColor = "rgba(58, 130, 238, 0.2)";
+            } else {
+                span.classList.add("business");
+                todoItem.style.backgroundColor = "rgba(241, 38, 72, 0.2)";
+            }
 
-        content.classList.add("todo-content");
-        actions.classList.add("actions");
-        editBtn.classList.add("edit");
-        deleteBtn.classList.add("delete");
+            content.classList.add("todo-content");
+            actions.classList.add("actions");
+            editBtn.classList.add("edit");
+            deleteBtn.classList.add("delete");
 
-        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-        editBtn.innerHTML = "Edit";
-        deleteBtn.innerHTML = "Delete";
+            content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+            editBtn.innerHTML = "Edit";
+            deleteBtn.innerHTML = "Delete";
 
-        label.appendChild(input);
-        label.appendChild(span);
-        actions.appendChild(editBtn);
-        actions.appendChild(deleteBtn);
-        todoItem.appendChild(label);
-        todoItem.appendChild(content);
-        todoItem.appendChild(actions);
+            label.appendChild(input);
+            label.appendChild(span);
+            actions.appendChild(editBtn);
+            actions.appendChild(deleteBtn);
+            todoItem.appendChild(label);
+            todoItem.appendChild(content);
+            todoItem.appendChild(actions);
 
-        todoList.appendChild(todoItem);
-
-        if (todo.done) {
-            todoItem.classList.add("done");
-            todoItem.style.backgroundColor = "rgb(180, 175, 175, 0.2)";
-        }
-        input.addEventListener("click", (e) => {
-            todo.done = e.target.checked;
-            localStorage.setItem("todos", JSON.stringify(todos));
+            todoList.appendChild(todoItem);
 
             if (todo.done) {
                 todoItem.classList.add("done");
-            } else {
-                todoItem.classList.remove("done");
+                todoItem.style.opacity = "0.6";
+                todoItem.style.backgroundColor = "rgb(180, 175, 175, 0.3)";
             }
+            input.addEventListener("click", (e) => {
+                todo.done = e.target.checked;
+                localStorage.setItem("todos", JSON.stringify(todos));
 
-            displayTodos();
-        });
-        editBtn.addEventListener("click", (e) => {
-            const input = content.querySelector("input");
-            input.removeAttribute("readonly");
-            input.focus();
-            input.addEventListener("blur", (e) => {
-                input.setAttribute("readonly", true);
-                todo.content = e.target.value;
+                if (todo.done) {
+                    todoItem.classList.add("done");
+                } else {
+                    todoItem.classList.remove("done");
+                }
+
+                displayTodos();
+            });
+            editBtn.addEventListener("click", (e) => {
+                const input = content.querySelector("input");
+                input.removeAttribute("readonly");
+                editBtn.innerHTML = "Save";
+                input.focus();
+                input.addEventListener("blur", (e) => {
+                    input.setAttribute("readonly", true);
+                    todo.content = e.target.value;
+                    localStorage.setItem("todos", JSON.stringify(todos));
+                    displayTodos();
+                });
+            });
+
+            deleteBtn.addEventListener("click", (e) => {
+                todos = todos.filter((t) => t !== todo);
                 localStorage.setItem("todos", JSON.stringify(todos));
                 displayTodos();
             });
         });
-
-        deleteBtn.addEventListener("click", (e) => {
-            todos = todos.filter((t) => t !== todo);
-            localStorage.setItem("todos", JSON.stringify(todos));
-            displayTodos();
-        });
-    });
 }
